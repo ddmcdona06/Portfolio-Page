6cd3ff34d548d9dbee564ccaf3319cb7cd7b76a0
ddmcdona06 <darryldmcdonald@gmail.com>
2024-01-18 11:21:44 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './components/Home';
+import logo3 from './img/cropped port.png'
+import PortDrawer from './components/Drawer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home/>
+        <img src={logo3} className="App-logo" alt="logo" />
+        <PortDrawer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Stack from '@mui/material/Stack';
+
+
+function Home() {
+
+
+
+  return (
+    <div>
+      <Stack spacing={-0.0015} >
+        <h1 className="home-name">Darryl McDonald</h1>
+        <h5 className="home-title" >Fullstack Developer</h5>
+      </Stack>
+    </div>
+  )
+}
+export default Home
