711f4d59c8572932ad9d3104d448c990ae316392
ddmcdona06 <darryldmcdonald@gmail.com>
2024-03-03 15:57:54 -0600

    built server with node and express

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const port = process.env.port || 4000;
+const distPath = path.resolve(__dirname, '..', 'build');
+
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(distPath))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(distPath, 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  })
+})
+app.listen(port, () => {
+  console.log(`listening at: http://localhost:${port}`)
+})
